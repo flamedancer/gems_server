cdf279e0c4edf6211f287974fbd4a9e920afe018
admin <admin@admindeiMac-2.local>
2015-08-13 12:04:50 +0800

    enemy api add doc

--- logics/enemy.py
+++ logics/enemy.py
@@ -9,6 +9,23 @@ def api_enemy():
     """ api/enemy/enemy
     
     获取敌将编队
+    Returns:
+        [
+            "1_card",    #  卡片id
+            3            #  开片level
+        ],
+        [
+            "1_card",
+            3
+        ],
+        [
+            "1_card",
+            3
+        ],
+        [
+            "1_card",
+            3
+        ]
     """
     return get_config_dir('userInit_config').get('enemy_team', {})
     
