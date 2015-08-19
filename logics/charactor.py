49d3f8bfcf205297080b959684afc3f94f3e1a90
admin <admin@admindeiMac-2.local>
2015-08-19 18:03:59 +0800

    modify add nature logic

--- logics/charactor.py
+++ logics/charactor.py
@@ -15,22 +15,30 @@ def api_rename(new_name):
     return {}
 
 
-def api_allot_nature(nature_type, num):
+def api_allot_nature(type_num):
     """ api/charactor/allot_nature
     元素掌握度加点
-
-    Args:
-        natur_type(int): 元素类别
             蓝  0
             红  1
             绿  2
             褐  3
             黄  4
             紫  5
-        num(int): 要加的点数
+
+    Args:
+        type_num(dir): 元素类别-要加的数量
+         例如：
+            {
+                0: 1,
+                1: 4,
+                3: 2,
+                ....
+            }
     """
     ubase = request.user
-    tools.del_user_things(ubase, 'nature_remain', num) 
-    tools.add_user_things(ubase, "nature_" + str(nature_type), num)
+    need_remain_num = sum(type_num.keys())
+    tools.del_user_things(ubase, 'nature_remain', need_remain_num) 
+    for nature_type, num in type_num.items():
+        tools.add_user_things(ubase, "nature_" + str(nature_type), num)
     return {}
         
