9bb13b1080287e5ded6cedddaa45578351fccb8e
admin <admin@admindeiMac-2.local>
2015-09-08 11:51:49 +0800

    json only support string key

--- logics/charactor.py
+++ logics/charactor.py
@@ -20,20 +20,20 @@ def api_rename(new_name):
 def api_allot_nature(type_num):
     """ api/charactor/allot_nature
     元素掌握度加点
-            蓝  0
-            红  1
-            绿  2
-            褐  3
-            黄  4
-            紫  5
+            蓝 "0"
+            红 "1"
+            绿 "2"
+            褐 "3"
+            黄 "4"
+            紫 "5"
 
     Args:
         type_num(dir): 元素类别-要加的数量
          例如：
             {
-                0: 1,
-                1: 4,
-                3: 2,
+                "0": 1,
+                "1": 4,
+                "3": 2,
                 ....
             }
     """
@@ -41,6 +41,6 @@ def api_allot_nature(type_num):
     need_remain_num = sum(type_num.keys())
     tools.del_user_things(ubase, 'nature_remain', need_remain_num) 
     for nature_type, num in type_num.items():
-        tools.add_user_things(ubase, "nature_" + str(nature_type), num)
+        tools.add_user_things(ubase, "nature_" + nature_type, num)
     return {}
         
