aac1bf1d568e3292f4f96cda91fbeb93ee66a718
admin <admin@admindeiMac-2.local>
2015-09-11 14:23:38 +0800

    cards modified info

--- common/tools.py
+++ common/tools.py
@@ -39,7 +39,7 @@ def del_user_things(user, thing, num, way):
         return new_num
     elif thing.endswith('_card'):
         new_card_info = user.user_cards.del_card(thing, num)
-        umodified.set_modify_info('cards', new_card_info)
+        umodified.set_modify_info('cards', {thing: new_card_info})
         change_info = new_card_info
     return change_info
     print "******del thing num way", thing, num, way 
