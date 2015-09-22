da106947ba3fad56257e84e994fb75f87e0d6416
admin <admin@admindeiMac-2.local>
2015-09-22 14:49:12 +0800

    add logic gacha for test

--- logics/gacha.py
+++ logics/gacha.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 """ 抽卡
 """
+#from common.utils import get_items_by_weight_dict
+from common.tools import *
+
 
 
 def api_coin_gacha():
     """ api/gacha/coin_gacha
-    用金币抽卡
+    用金币抽卡,获得三张卡牌,必有一张绿卡
 
     Returns:
        get_cards(list): 获得的卡牌序列 
     """
-    return {'get_cards': ['1_card', '2_card', '3_card']}
+    user = request.user
+    get_cards = ['1_card', '2_card', '3_card']
+    for card_id in get_cards:
+        add_user_things(user, card_id, 1, 'coin_gacha')
+    return {'get_cards': get_cards}
 
 
 def api_diamond_gacha():
